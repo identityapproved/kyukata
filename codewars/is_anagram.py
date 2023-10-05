837d6ba84f2677397324de236bce93f6800086ad
identityapproved <identityapproved@gmail.com>
2023-10-05 15:33:21 +0300

    testing and preparing

---
+++ codewars/is_anagram.py
@@ -0,0 +1,18 @@
+# write the function is_anagram
+def is_anagram(test, original):
+    #test_set = set([i for i in test.lower()])
+    #original_set = set([i for i in original.lower()])
+    #if test_set.issuperset(original_set):
+        #return True
+    #return False
+    t_set = sorted([i for i in test.lower()])
+    o_set = sorted([i for i in original.lower()])
+    print(t_set)
+    print(o_set)
+
+print(is_anagram("foefet", "toffee"), True, 'The word foefet is an anagram of toffee')
+print(is_anagram("Buckethead", "DeathCubeK"), True, 'The word Buckethead is an anagram of DeathCubeK')
+print(is_anagram("Twoo", "WooT"), True, 'The word Twoo is an anagram of WooT')
+print(is_anagram("dumble", "bumble"), False, 'Characters do not match for test case dumble, bumble')
+print(is_anagram("ound", "round"), False, 'Missing characters for test case ound, round')
+print(is_anagram("apple", "pale"), False, 'Same letters, but different count')
---
+++ leetcode/2235.py
@@ -0,0 +1,4 @@
+def sum(num1: int, num2: int) -> int:
+    return num1+num2
+
+print(sum(23,23))
