e8c51b9906be54257d4ad83e456429403258e850
identityapproved <identityapproved@gmail.com>
2023-10-30 13:52:02 +0200

    Boiled Eggs(7 kyu) with tests.

--- codewars/__tests__.py
+++ codewars/__tests__.py
@@ -1,7 +1,9 @@
 import unittest
 from squaresum import square_sum 
-# square_sum unittest asserts
+from boiledeggs import cooking_time 
+
 class TestCodewars(unittest.TestCase):
+
     def test_square_sum_assertions(self):
         self.assertEqual(square_sum([1,2]), 5)
         self.assertEqual(square_sum([0, 3, 4, 5]), 50)
@@ -9,5 +11,12 @@ class TestCodewars(unittest.TestCase):
         self.assertEqual(square_sum([-1,-2]), 5)
         self.assertEqual(square_sum([-1,0,1]), 2)
 
+    def test_cooking_time_assertions(self):
+        self.assertEqual(cooking_time(0), 0) 
+        self.assertEqual(cooking_time(5), 5) 
+        self.assertEqual(cooking_time(10), 10) 
+        self.assertEqual(cooking_time(18710), 21045) 
+        self.assertEqual(cooking_time(34181), 21365) 
+        
 if __name__ == '__main__':
     unittest.main()
